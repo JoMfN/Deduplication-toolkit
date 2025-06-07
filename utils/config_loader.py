70971dbb919ad4b87d0260a1166f9623f03b4f3c
Joachim Snellings <joachim.snellings@mfn.berlin>
2025-06-07 23:52:19 +0200

    Update config_loader.py

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -2,7 +2,7 @@ import os
 import yaml
 
 def load_config(args):
-    \"\"\"Load YAML config and return as a dictionary.\"\"\"
+    """Load YAML config and return as a dictionary."""
     if getattr(args, 'config', None) and os.path.exists(args.config):
         with open(args.config, 'r') as f:
             return yaml.safe_load(f)
